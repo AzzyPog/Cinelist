b4373f7ba13bf8296af5fc653d0b39b0660c4ff4
Renato <renato@ejcm.com.br>
2023-03-09 01:57:27 -0300

    refactor: algumas mudanças feitas no card

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,26 @@
 
 
+//adquire todos os dados da api e converte para json, retornando só a parte da response que importa nesse momento
 async function getPopularFilms() {
     try {
         const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=5c86de7f8c69f21a842fb268d9a8fb37&language=pt-BR&page=1', {
             method: 'GET'
         });
-        var Films = await res.json();
+        let Films = await res.json();
         Films = Films.results;
-        console.log(Films)
+
+        return Films;
 
     } catch (error) {
         console.log(error);
     }
+};
 
-}
-getPopularFilms();
+async function showFilmData() {
+    try {
+        const films = await getPopularFilms();
+    } catch (error) {
+
+    }
+} showFilmData();
+
